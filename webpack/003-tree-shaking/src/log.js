11effc6594df3e8fa2c6b4bf1faa1bee7714112d
Evan Boissonnot <evan@boissonnot.fr>
2020-03-13 15:26:23 +0100

    evo: demo of tree shaking

---
+++ webpack/003-tree-shaking/src/log.js
@@ -0,0 +1,11 @@
+export function log(value) {
+    console.log(value);
+}
+
+export function log1(value) {
+    console.info(value);
+}
+
+export function log2(value) {
+    console.error(value);
+}
