4ede10aa60d3684ae8af31485a30189767f3b533
Evan Boissonnot <evan@boissonnot.fr>
2020-03-13 18:29:50 +0100

    evo: loading json file as object

---
+++ webpack/0061-mode-loader-json/src/index.js
@@ -0,0 +1,7 @@
+import { log, log1, log2 } from './log'
+import config from './config.json'
+
+log('ah que coucou');
+log('ohoho');
+
+console.log(config);
---
+++ webpack/0061-mode-loader-json/src/log.js
@@ -0,0 +1,12 @@
+export function log(value) {
+    console.log(value);
+    const coucou = 1;
+}
+
+export function log1(value) {
+    console.info(value);
+}
+
+export function log2(value) {
+    console.error(value);
+}
