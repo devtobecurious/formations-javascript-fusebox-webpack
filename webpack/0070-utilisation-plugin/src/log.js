83cf4271b59e286576c8fae059aea76f9bbc82b3
Evan Boissonnot <evan@boissonnot.fr>
2020-03-15 15:53:48 +0100

    evo: using html-webpack-plugin

---
+++ webpack/0070-utilisation-plugin/src/log.js
@@ -0,0 +1,14 @@
+export function log(value) {
+    console.log(value);
+    const coucou = 1;
+}
+
+export function log1(value) {
+    console.info(value);
+}
+
+export function log2(value) {
+    console.error(value);
+}
+
+// export fnuc() {} // If uncomment, no main.js will be generated
---
+++ webpack/0070-utilisation-plugin/webpack.config.js
@@ -0,0 +1,33 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  optimization: {
+    noEmitOnErrors: true
+  },
+  watch: true,
+  plugins: [
+    new HtmlWebpackPlugin(), // Generates default index.html
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      filename: 'test.html',
+      template: 'src/assets/test.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  }
+};
