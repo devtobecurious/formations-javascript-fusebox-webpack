7935faa284e0cb6d7a82b095dc3cb25901e779f9
evan-boissonnot <commercial@boissonnot.Fr>
2020-03-16 10:16:23 +0100

    fix: lazy loading is ok

--- webpack/0090-lazy-loading/src/index.js
+++ webpack/0090-lazy-loading/src/index.js
@@ -1,7 +1,5 @@
-import { log, log1, log2 } from './log'
-import config from './config.json'
-
-import $ from 'jquery';
+import { log, log1, log2 } from './log';
+import config from './config.json';
 
 
 log('ah que coucou');
@@ -9,8 +7,26 @@ log('ohoho');
 
 console.log(config);
 
-$.ajax('https://swapi.co/api/people/', {
-    success: (data) => {
-        log(data);
-    }
-});
+
+
+const button = document.getElementById('clickme');
+
+// https://babeljs.io/docs/en/babel-plugin-syntax-dynamic-import/
+// npm install --save-dev @babel/plugin-syntax-dynamic-import
+
+button.addEventListener('click', (e) => {
+    console.log('coucou');
+    import(/* webpackChunkName: "jquery" */ 'jquery').then((jQuery) => {
+        window.$ = jQuery.default;
+        //window.jQuery = jQuery;
+        console.log(jQuery);
+
+        window.$('body').css('backgroundColor', '#00FF00');
+        
+        window.$.ajax('https://swapi.co/api/people/', {
+            success: (data) => {
+                log(data);
+            }
+        });
+    });
+})
