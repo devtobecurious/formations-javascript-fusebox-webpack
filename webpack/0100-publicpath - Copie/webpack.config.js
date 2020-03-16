256d8ef4242f53335260bd11662c5a259f7ebb5c
Evan Boissonnot <evan@boissonnot.fr>
2020-03-16 12:37:59 +0100

    evo: managing publicpath

---
+++ webpack/0100-publicpath - Copie/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/'
+  },
+  mode: 'development',
+  optimization: {
+    noEmitOnErrors: true
+  },
+  watch: true,
+  plugins: [
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  }
+};
