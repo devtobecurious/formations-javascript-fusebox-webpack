e078298504dbec4c4d438454df42765253341835
Evan Boissonnot <evan@boissonnot.fr>
2020-03-16 15:31:46 +0100

    evo: clean before compiling

---
+++ webpack/0103-clean-before-compiling - Copie/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[hash:8].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  optimization: {
+    noEmitOnErrors: true
+  },
+  watch: true,
+  plugins: [
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  }
+};
