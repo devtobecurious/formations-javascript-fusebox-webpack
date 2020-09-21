10249008d29903569dee5ffb0cf4ec64ec3db7d0
evan@boissonnot.fr <Bienvenue_2510?>
2020-09-21 14:12:18 +0200

    evo: first configuration bis

---
+++ fusebox/002-decouverte-configuration/fuse.js
@@ -0,0 +1,21 @@
+const { FuseBox, WebIndexPlugin } = require("fuse-box");
+const { isConstructorDeclaration } = require("typescript");
+
+const fuse = FuseBox.init({
+    homeDir: "src",
+    output: "build/$name.js",
+    cache: true,
+    plugins: [WebIndexPlugin()],
+    tsConfig: "./tsconfig.json"
+  });
+
+  fuse
+  .bundle("test")
+  .instructions("> index.ts")
+  .hmr();
+
+fuse.dev({
+  port: 4309
+});
+  
+fuse.run().then(() => console.log('finish'));
---
+++ fusebox/002-decouverte-configuration/src/index.ts
@@ -0,0 +1,3 @@
+class Wookie {
+    name: string;
+}
